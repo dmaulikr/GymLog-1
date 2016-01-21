3481ac7fb53f088e8c0930a0f17417471319720f
Feifan Zhou <feifan@me.com>
2016-01-20 16:38:27 -0800

    Show correct amount of gym time this week in workouts summary

--- ios/WorkoutsSummary.m
+++ ios/WorkoutsSummary.m
@@ -11,6 +11,8 @@
 
 @interface WorkoutsSummary()
 @property (strong, nonatomic) IBOutlet UILabel *durationLabel;
+
++ (NSDate *)firstDayOfWeekFrom:(NSDate *)givenDate;
 @end
 
 @implementation WorkoutsSummary
@@ -20,12 +22,25 @@
     return nil;
   
   int totalDuration = 0;
+  NSTimeInterval weekStart = [[self firstDayOfWeekFrom:[NSDate date]] timeIntervalSince1970];
   for (WorkoutMO *workout in workouts)
-    totalDuration += ((int)workout.workoutEnd - (int)workout.workoutStart);
+    if (workout.workoutStart >= weekStart)
+      totalDuration += ((int)workout.workoutEnd - (int)workout.workoutStart);
   int totalHours = totalDuration / 3600;
   int totalMinutes = (totalDuration - totalHours * 3600) / 60;
   summary.durationLabel.text = [NSString stringWithFormat:@"%dh %dm", totalHours, totalMinutes];
   
   return summary;
 }
+
++ (NSDate *)firstDayOfWeekFrom:(NSDate *)givenDate {
+  NSCalendar *calendar = [NSCalendar currentCalendar];
+  
+  NSDateComponents *components = [calendar components:NSYearCalendarUnit|NSMonthCalendarUnit|NSWeekCalendarUnit|NSWeekdayCalendarUnit fromDate:givenDate];
+  [components setWeekday:1]; // 1 == Sunday, 7 == Saturday
+  [components setWeekOfYear:[components weekOfYear]];
+  
+  return [calendar dateFromComponents:components];
+}
+
 @end
